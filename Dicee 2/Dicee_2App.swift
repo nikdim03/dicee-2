1c80c59c8d2de5bad6bb8a94105736d94afd456a
Dmitriy Nikulin <nikdim_03@mail.ru>
2022-03-27 18:16:09 +0300

    Initial Commit

---
+++ Dicee 2/Dicee_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_2App.swift
+//  Dicee 2
+//
+//  Created by Dmitriy on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
